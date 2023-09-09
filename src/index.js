afe6727e695a93e18f2faaf2cc5a5c4ab26c5fef
simplyratl <nikicaraznatovic123@gmail.com>
2023-09-10 01:32:48 +0200

    update api

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/song", songRouter);
+app.use("/api/song", songRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello");
